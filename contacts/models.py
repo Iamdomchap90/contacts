2c8b503ef6a4a39462e28889608ba55ea5dd1e06
Dominic Chaple <d0mch4pl3@gmail.com>
2025-02-23 19:04:28 +0000

    Add contact functionality.

--- contacts/models.py
+++ contacts/models.py
@@ -8,7 +8,11 @@ class Contact(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
+    address = models.TextField(blank=True, default='')
+    phone_number = models.CharField(max_length=20, blank=True)
     company = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
