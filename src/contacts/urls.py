51097c215f7a63b8f66e5321905b09a78f5781a7
Dominic Chaple <d0mch4pl3@gmail.com>
2025-02-24 00:53:18 +0000

    Custom js added to toggle modal and htmx implemented for ajax call to delete view.

--- src/contacts/urls.py
+++ src/contacts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from contacts.views import ContactFormView, ContactListView, ContactUpdateView
+from contacts.views import ContactFormView, ContactListView, ContactUpdateView, delete_contact
 
 
 app_name = "contacts"
@@ -9,4 +9,5 @@ urlpatterns = [
     path("", ContactListView.as_view(), name="list"),
     path("add/", ContactFormView.as_view(), name="add"),
     path("update/<int:pk>/", ContactUpdateView.as_view(), name="update"),
+    path("delete/<int:pk>/", delete_contact, name="delete"),
 ]
